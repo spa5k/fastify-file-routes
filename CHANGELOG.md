0e42d3f3f61a5d9d5e44be2a7b1a88a578ee1973
Spa5k <79936503+spa5k@users.noreply.github.com>
2021-12-21 15:16:22 +0530

    fix(autoload): 🐛 adding a try catch to make sure that wrong route does not result in a crash

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,9 +1,8 @@
 ## [1.0.2](https://github.com/spa5k/fastify-file-routes/compare/v1.0.1...v1.0.2) (2021-12-21)
 
-
 ### Bug Fixes
 
-* **deps:** 🐛 moving fastify-plugin to dependency to fix missing dep error ([b9b956f](https://github.com/spa5k/fastify-file-routes/commit/b9b956f2476b9132fc8f2678ea01f74279a74a4b))
+- **deps:** 🐛 moving fastify-plugin to dependency to fix missing dep error ([b9b956f](https://github.com/spa5k/fastify-file-routes/commit/b9b956f2476b9132fc8f2678ea01f74279a74a4b))
 
 ## [1.0.1](https://github.com/spa5k/fastify-file-routes/compare/v1.0.0...v1.0.1) (2021-12-21)
 
--- src/utils/autoload.ts
+++ src/utils/autoload.ts
@@ -13,35 +13,45 @@ export async function autoload(
 ): Promise<void> {
   const module: ModuleType = await loadModule(fullPath);
 
-  for (const [method, route] of Object.entries<RouteOptions>(module)) {
-    if (validMethods.has(method)) {
-      route.url = prefix ? `/${prefix}${url}` : url;
-      route.method = method.toUpperCase() as ValidMethods;
-      route.prefixTrailingSlash = "both";
+  try {
+    for (const [method, route] of Object.entries<RouteOptions>(module)) {
+      if (validMethods.has(method)) {
+        route.url = prefix ? `/${prefix}${url}` : url;
+        route.method = method.toUpperCase() as ValidMethods;
+        route.prefixTrailingSlash = "both";
 
-      fastify.route(route);
-      switch (route.method) {
-        case "POST": {
-          console.info(`${pc.yellow(method.toUpperCase())}      ${route.url}`);
+        fastify.route(route);
+        switch (route.method) {
+          case "POST": {
+            console.info(
+              `${pc.yellow(method.toUpperCase())}      ${route.url}`
+            );
 
-          break;
-        }
-        case "HEAD": {
-          console.info(
-            `${pc.bgMagenta(method.toUpperCase())}      ${route.url}`
-          );
+            break;
+          }
+          case "HEAD": {
+            console.info(
+              `${pc.bgMagenta(method.toUpperCase())}      ${route.url}`
+            );
 
-          break;
-        }
-        case "OPTIONS": {
-          console.info(`${pc.bgCyan(method.toUpperCase())}   ${route.url}`);
+            break;
+          }
+          case "OPTIONS": {
+            console.info(`${pc.bgCyan(method.toUpperCase())}   ${route.url}`);
 
-          break;
-        }
-        default: {
-          console.info(`${pc.green(method.toUpperCase())}       ${route.url}`);
+            break;
+          }
+          default: {
+            console.info(
+              `${pc.green(method.toUpperCase())}       ${route.url}`
+            );
+          }
         }
       }
     }
+  } catch (err) {
+    throw new Error(
+      `Error loading module at ${fullPath}, check if this file is a correct route module or not`
+    );
   }
 }
